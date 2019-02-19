b126d3b8e0a02333a5be74c1cde5336fba639f4d
Louis Liao <buzonliao@gmail.com>
2019-02-19 18:24:55 +0800

    add test

--- cards/main.go
+++ cards/main.go
@@ -1,15 +1,15 @@
 package main
 
 func main()  {
-	// cards := newDeck()
-
+	cards := newDeck()
+	cards.shuffle()
 	// cards.saveToFile("my_cards")
-	results := newDeckFromFile("my_cards")
-	results.print()
+	// results := newDeckFromFile("my_cards")
+	// results.print()
 	// hand, remianingCards := deal(cards, 5)
 	
 	// hand.print()
 	// remianingCards.print()
-
+	cards.print()
 }
 
